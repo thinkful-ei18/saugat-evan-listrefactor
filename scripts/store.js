cf7beb2acccfa07951353e74fd0da00dfa71ea7d
Saugat Dhungel <saugat.dhungel@gmail.com>
2018-01-22 11:37:11 -0500

    created a  item and a store module

---
+++ scripts/store.js
@@ -0,0 +1,19 @@
+/* global store, cuid */
+
+'use strict'
+
+
+const store = (function () {
+  
+  return {
+    items: [
+      { id: cuid(), name: 'apples', checked: false },
+      { id: cuid(), name: 'oranges', checked: false },
+      { id: cuid(), name: 'milk', checked: true },
+      { id: cuid(), name: 'bread', checked: false }
+    ],
+    hideCheckedItems: false,
+    searchTerm: ''
+  };
+}());
+
